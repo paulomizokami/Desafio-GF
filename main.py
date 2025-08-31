deae73e34c6cb4d54525ae3e7063b37fc1924665
Paulo Mizokami <hiroche.mizokami1@gmail.com>
2025-08-31 15:25:54 -0300

    commit 2

--- main.py
+++ main.py
@@ -3,10 +3,17 @@ import pandas as pd
 import numpy as np
 
 # Carregar os arquivos
-clientes = pd.read_csv("C:/Users/AMD/Documents/Projeto_GF/arq/desafio_gerando_falcoes/GF/clientes.csv")
-#vendas = pd.read_csv("data/vendas.csv")
-#produtos = pd.read_csv("data/produtos.csv")
+client = pd.read_csv("data/clientes.csv")
+sales = pd.read_csv("data/vendas.csv")
+product = pd.read_csv("data/produtos.csv")
 
 # Visualizar as 5 primeiras linhas de cada base
 print("Clientes:")
-print(clientes.head())
+print(client.head())
+
+print("Vendas:")
+print(sales.head())
+
+print("Produtos:")
+print(product.head())
+
